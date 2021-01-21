b602c83f7b8d97b39393e1ea0b7d9ddb32b1e2ce
Alikhan Askarov <aaskarov@kaptechnology.kazatomprom.kz>
2021-01-21 19:06:58 +0600

    WIP

--- __tests__/loader.test.js
+++ __tests__/loader.test.js
@@ -8,11 +8,15 @@ import os from 'os';
 import _ from 'lodash';
 import loadPage from '../src/index.js';
 
+
 nock.disableNetConnect();
 
 let pageCode;
-let scope;
+let firstTestScope;
+let secondTestScope;
 let testFilePath;
+let assetFilePath;
+let imageData;
 
 const getFixturePath = (filename, extension) => {
   const dirname = path.resolve();
@@ -21,19 +25,30 @@ const getFixturePath = (filename, extension) => {
 
 beforeAll(async () => {
   pageCode = await fs.readFile(getFixturePath('page', 'html'), 'utf-8');
+  imageData = await fs.readFile(getFixturePath('hexlet-io-assets-professions-nodejs', 'png'), 'utf-8');
   testFilePath = path.join(os.tmpdir(), 'hexlet-io-courses.html');
-  scope = nock('https://hexlet.io')
+  assetFilePath = path.join(os.tmpdir(), 'hexlet-io-courses_files', 'hexlet-io-assets-professions-nodejs.png');
+  firstTestScope = nock('https://hexlet.io')
     .get('/courses')
     .reply(200, pageCode.trim());
+  secondTestScope = nock('https://hexlet.io')
+    .get('/image/1')
+    .reply(200, imageData.trim());
 });
 
 beforeEach(async () => {
   await fs.unlink(testFilePath).catch(_.noop);
 });
 
-describe('test', () => {
-  test('same content', () => loadPage('https://hexlet.io/courses', os.tmpdir())
+describe('basic tests', () => {
+  test('simple page download', () => loadPage('https://hexlet.io/courses', os.tmpdir())
     .then(() => fs.readFile(testFilePath, 'utf-8'))
     .then((data) => expect(data.trim()).toEqual(pageCode.trim()))
-    .then(() => expect(scope.isDone()).toBe(true)));
+    .then(() => expect(firstTestScope.isDone()).toBe(true)));
+
+  test('should download img', () => loadPage('https://hexlet.io/image/1', os.tmpdir())
+    .then(() => fs.readFile(assetFilePath, 'utf-8'))
+    .then((data) => expect(data.trim()).toEqual(imageData.trim()))
+    .then(() => expect(secondTestScope.isDone()).toBe(true)));
+
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import fs from 'fs/promises';
 import path from 'path';
+import cheerio from 'cheerio';
 
 const urlToFileName = (url) => {
   const rule = /\/\/(.*)/;
@@ -9,8 +10,28 @@ const urlToFileName = (url) => {
     .flatMap((word) => word.split('.'));
   return arrayOfWords.join('-').concat('.html');
 };
+const urlToAssetDirectory = (url) => {
+  const rule = /\/\/(.*)/;
+  const arrayOfWords = url.match(rule)[0].split('/')
+    .filter((word) => word)
+    .flatMap((word) => word.split('.'));
+  return arrayOfWords.join('-').concat('_files');
+};
+const getImageLinksFromPage = (page) => {
+  const $ = cheerio.load(page);
+  const elements = Array.from($('img[src]'));
+  const links = elements.map((element) => element.src);
+  return links;
+};
 export default async (url, savePath) => {
-  const outputFilename = urlToFileName(url);
-  const resultFilename = path.join(savePath, outputFilename);
-  return axios.get(url).then((content) => fs.writeFile(resultFilename, content.data));
+  let imageLinks;
+  const pageFilename = urlToFileName(url);
+  const assetsDirectory = urlToAssetDirectory(url);
+  const resultPath = path.join(savePath, pageFilename);
+  const request = axios.get(url).then((content) => {
+    imageLinks = getImageLinksFromPage(content.data);
+    return content;
+  }).then((content) => fs.writeFile(resultPath, content.data));
+
+  // const request = axios.get(url).then((content) => fs.writeFile(resultFilename, content.data));
 };
