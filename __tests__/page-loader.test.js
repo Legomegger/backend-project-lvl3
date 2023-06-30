2a6992c91254d699d74cd898015bb8ccef6d8160
Askarov Alikhan <ali@powr.io>
2023-06-30 17:30:46 +0600

    step 2

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -5,6 +5,7 @@ import path from 'path';
 import axios from 'axios';
 import os from 'os';
 import _ from 'lodash';
+import * as cheerio from 'cheerio';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -12,30 +13,31 @@ const __dirname = path.dirname(__filename);
 const MOCK_URL = "https://ru.hexlet.io";
 
 nock.disableNetConnect();
+
 let scope;
 let tmpDir;
 let fakeHexletContent;
-let fullResultingPath
-beforeAll(() => {
-  tmpDir = os.tmpdir()
+let fullResultingPath;
+
+const getFixturesPath = (name) => path.join(__dirname, '__fixtures__', name);
+
+beforeEach(async () => {
+  tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
   scope = nock(MOCK_URL)
     .persist()
     .get('/courses')
-});
-
-beforeEach(async () => {
-  await fs.unlink(`${tmpDir}/ru-hexlet-io-courses.html`).catch(_.noop)
-  scope.replyWithFile(200, `${__dirname}/__fixtures__/fake-hexlet-content.html`)
-  fakeHexletContent = await fs.readFile(getFixturesPath('fake-hexlet-content.html'), 'utf-8');
+    .replyWithFile(200, getFixturesPath('fake-hexlet-content.html'))
+    .get('/assets/professions/nodejs.png')
+    .replyWithFile(200, getFixturesPath('node.png'));
+  fakeHexletContent = (await fs.readFile(getFixturesPath('fake-hexlet-content.html'), 'utf-8')).trim()
   fullResultingPath = await pageLoader(`${MOCK_URL}/courses`, tmpDir);
 })
 
-const getFixturesPath = (name) => path.join(__dirname, '__fixtures__', name);
 
 describe('url is mocked', () => {
   test('and working', async () => {
     const response = await axios.get(`${MOCK_URL}/courses`);
-    expect(response.data).toEqual(fakeHexletContent);
+    expect(response.data.trim()).toEqual(fakeHexletContent);
   });
 })
 
@@ -43,7 +45,7 @@ describe('main flow', () => {
   test('save filename is correct', async () => {
     const resultFilename = 'ru-hexlet-io-courses.html';
     const dirContent = await fs.readdir(tmpDir);
-    expect(dirContent.includes(resultFilename))
+    expect(dirContent.includes(resultFilename)).toBeTruthy();
   });
 
   test('return value is correct', async () => {
@@ -53,6 +55,24 @@ describe('main flow', () => {
 
   test('content is being saved', async () => {
     const result = await fs.readFile(fullResultingPath, 'utf-8');
-    expect(result.trim()).toEqual(fakeHexletContent.trim());
+    expect(result.length).toBeGreaterThan(10);
+  });
+
+  test('assets dir is present', async () => {
+    const dirContent = await fs.readdir(tmpDir);
+    const assetsDirname = 'ru-hexlet-io-courses_files';
+    expect(dirContent.includes(assetsDirname)).toBeTruthy();
+  });
+
+  test('image is downloading', async () => {
+    const response = await axios.get('https://ru.hexlet.io/assets/professions/nodejs.png', { responseType: 'stream' } );
+    expect(response.data).toBeDefined();
+  });
+
+  test('img path should be from local assets dir', async () => {
+    const afterFixturePath = getFixturesPath('fake-hexlet-content-after-image-processing.html')
+    const after = cheerio.load((await fs.readFile(afterFixturePath, 'utf-8')).trim()).html();
+    const result = await fs.readFile(fullResultingPath, 'utf-8');
+    expect(result).toEqual(after);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import path from 'path';
 import axios from 'axios';
 import fs from 'fs/promises';
-import { urlToFilename } from '../lib/utils.js';
+import * as cheerio from 'cheerio';
+import { transformUrl } from '../lib/utils.js';
+import { createWriteStream } from 'fs';
 
 const pageLoader = (url, savePath = process.cwd()) => {
-  const filename = urlToFilename(url);
-  const savedPath = `${savePath}/${filename}`;
-  return axios.get(url)
+  const urlObject = new URL(url);
+  const urlTo = transformUrl(urlObject);
+  const assetsDirname = urlTo('_files');
+  const pageFilename = urlTo('.html');
+  const assetsDirFullpath = `${savePath}/${assetsDirname}`;
+
+  const savedPath = `${savePath}/${pageFilename}`;
+  let $;
+  let originalImageSrcs;
+  let promises;
+  let imgs;
+  let newAssetFilenames;
+  return axios.get(urlObject.href)
     .then((r) => r.data)
-    .then((data) => data.trim())
-    .then((content) => fs.writeFile(savedPath, content))
+    .then((data) => {
+      $ = cheerio.load(data.trim());
+      imgs = $('img');
+      originalImageSrcs = imgs.get().map((img) => {
+        return $(img).attr('src').startsWith('/') ? `${urlObject.origin}${$(img).attr('src')}`: `${$(img).attr('src')}`
+      });
+    })
+    .then(fs.mkdir(assetsDirFullpath))
+    .then(() => {
+      newAssetFilenames = originalImageSrcs.map((imgSrc) => {
+        return transformUrl(new URL(imgSrc))('');
+      });
+    })
+    .then(() => {
+      promises = originalImageSrcs.map((imgSrc) => {
+        return axios.get(imgSrc, { responseType: 'stream' });
+      })
+      return Promise.all(promises);
+    })
+    .then((contents) => {
+      contents.forEach((content, i) => {
+        const downloadedAssetFilename = newAssetFilenames[i]
+        content.data.pipe(createWriteStream(`${assetsDirFullpath}/${downloadedAssetFilename}`))
+        $(imgs.get()[i]).attr('src', `${assetsDirname}/${downloadedAssetFilename}`)
+      });
+    })
+    .then(() => fs.writeFile(savedPath, $.html()))
     .then(() => savedPath)
     .catch((e) => console.error(e));
 };
