ba005746c89c54b5193b390c36d642391c57335a
Ali Powr <alipowr@Alis-MacBook-Air.local>
2025-06-25 20:28:56 +0500

    step 2 needs refactor

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,23 +9,31 @@ nock.disableNetConnect();
 const baseUrl = 'https://kz.hexlet.io';
 const page = '/courses';
 const url = `${baseUrl}${page}`;
-const cleanTempFile = (filename) => async () => {
-  await fs.unlink(filename).catch(() => { });
-}
 
 describe('working with simple contents', () => {
-  let fixtureData;
+  let beforeFixtureData;
+  let afterFixtureData;
   let fullFilePath
+  let fullAssetsPath
   let tempdirName;
   const fileName = 'kz-hexlet-io-courses.html';
+  const imgDirName = 'kz-hexlet-io-courses_files'
 
   beforeEach(async () => {
     tempdirName = os.tmpdir()
     fullFilePath = `${tempdirName}/${fileName}`;
-    fixtureData = (await fs.readFile(getFixturePath('simple_contents.html'), 'utf8')).trim();
-    nock(baseUrl).get(page).reply(200, fixtureData);
+    fullAssetsPath = `${tempdirName}/${imgDirName}`;
+    await fs.unlink(fullFilePath).catch(() => { });
+    await fs.rm(fullAssetsPath, { recursive: true }).catch(() => { });
+    beforeFixtureData = (await fs.readFile(getFixturePath('before.html'), 'utf8')).trim();
+    nock(baseUrl).get(page).reply(200, beforeFixtureData);
+
+    const fixtureImagePath = getFixturePath('/assets/professions/nodejs.png');
+    const imageBuffer = await fs.readFile(fixtureImagePath);
+    nock(baseUrl).get('/assets/professions/nodejs.png').reply(200, imageBuffer, {
+      'Content-Type': 'image/png',
+    });
   })
-  beforeEach(cleanTempFile(fullFilePath));
 
   test('should create correct file', async () => {
     await loader(url, tempdirName);
@@ -35,6 +43,18 @@ describe('working with simple contents', () => {
   test('should save contents from url', async () => {
     await loader(url, tempdirName);
     const tempfileContents = (await fs.readFile(fullFilePath, 'utf8')).trim();
-    expect(tempfileContents).toEqual(fixtureData);
+    expect(tempfileContents).toEqual(beforeFixtureData);
+  });
+
+  test('should create files dir', async () => {
+    await loader(url, tempdirName);
+    const files = await fs.readdir(os.tmpdir());
+    expect(files).toContain(imgDirName);
+  });
+
+  test('should download image', async () => {
+    await loader(url, tempdirName);
+    const imagePath = `${tempdirName}/${imgDirName}/kz-hexlet-io-assets-professions-nodejs.png`;
+    await expect(fs.access(imagePath)).resolves.not.toThrow();
   });
 })
--- src/index.js
+++ src/index.js
@@ -1,18 +1,65 @@
 import axios from "axios"
 import * as fs from 'fs/promises';
+import path from "path";
+import * as cheerio from 'cheerio';
+import { createWriteStream } from 'fs';
 
-const urlToFilename = (url) => {
+
+// Базовая функция преобразования URL в строку
+const urlToBaseName = (url, params = {}) => {
   const transformedHostname = url.hostname.replaceAll('.', '-');
   const transformedPathname = url.pathname.replaceAll('/', '-');
-  return `${transformedHostname}${transformedPathname}.html`;
+  if (params.hostname) {
+    return `${transformedHostname}`;
+  } else if (params.pathname) {
+    return `${transformedPathname}`;
+  }
+  return `${transformedHostname}${transformedPathname}`;
 };
+
+// Функции-композиторы для добавления суффиксов
+const withHtmlExtension = (baseName) => `${baseName}.html`;
+const withFilesExtension = (baseName) => `${baseName}_files`;
+
+// Композиция функций для получения конечных имён
+const urlToFilename = (url) => withHtmlExtension(urlToBaseName(url));
+const urlToDirname = (url) => withFilesExtension(urlToBaseName(url));
+
+const extractImgSrc = (rawHtml) => {
+  const $ = cheerio.load(rawHtml);
+  const imgSrc = $('img').attr('src');
+  return imgSrc;
+};
+
 export default (url, outputDir) => {
   const urlObject = new URL(url);
   const filename = urlToFilename(urlObject);
-  return axios.get(url)
-    .then((response) => {
-      return response.data;
-    }).then((contents) => {
-      return fs.writeFile(`${outputDir}/${filename}`, contents);
-    });
+  const imgDirName = urlToDirname(urlObject);
+  let imgSrc;
+
+  return fs.mkdir(path.join(outputDir, imgDirName), { recursive: true })
+    .then(() => axios.get(url))
+    .then((response) => response.data)
+    .then((contents) => {
+      imgSrc = extractImgSrc(contents);
+      return contents;
+    })
+    .then((contents) => fs.writeFile(path.join(outputDir, filename), contents))
+    .then(() => {
+      const hostname = urlObject.hostname
+      const protocol = urlObject.protocol
+      const imgPath = path.join(protocol, hostname, imgSrc)
+      return axios.get(imgPath, {
+        responseType: 'stream'
+      })
+    })
+    .then((imageContents) => {
+      const imgSavePath = path.join(outputDir, imgDirName, 'kz-hexlet-io-assets-professions-nodejs.png')
+      const writer = createWriteStream(imgSavePath);
+      imageContents.data.pipe(writer);
+      return new Promise((resolve, reject) => {
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+      });
+    })
 }
